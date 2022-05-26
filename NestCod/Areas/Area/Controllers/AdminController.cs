1002ce9b20e03b6f7d7d6e8ff9b32134c177d708
416Shemi <tu7ngcsvr@code.edu.az>
2022-05-26 23:47:38 +0400

    new projekt

---
+++ NestCod/Areas/Area/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NestCod.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NestCod.Areas.Area.Controllers
+{
+    [Area("Area")]
+    public class AdminController : Controller
+    {
+        private AppDbContext _Context { get; }
+        public AdminController(AppDbContext Context)
+        {
+            _Context = Context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+      
+    }
+}
